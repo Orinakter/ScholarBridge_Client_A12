50ace7825df3b388061220d55154520b35b74fd2
Orinakter <orin.akter9013@gmail.com>
2025-01-18 00:28:12 +0600

    all schoolarship button addedd in homepage

---
+++ src/Components/AllSchoolarship.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const AllSchoolarship = () => {
+    return (
+        <div className='text-center mt-24'>
+           <Link to="/all-scholarship"> <button className='btn bg-[#126e82] text-white font-bold text-2xl '>All Schoolarship</button></Link>
+        </div>
+    );
+};
+
+export default AllSchoolarship;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../HomeLayOut/Banner';
 import TopScholarShip from '../Components/TopScholarShip';
+import AllSchoolarship from '../Components/AllSchoolarship';
 
 const HomePage = () => {
     return (
@@ -8,6 +9,7 @@ const HomePage = () => {
             <Banner></Banner>
             <div className="space-y-12 2xl:space-y-28 py-12 2xl:py-28">
             <TopScholarShip></TopScholarShip>
+            <AllSchoolarship></AllSchoolarship>
             </div>
         </div>
     );
